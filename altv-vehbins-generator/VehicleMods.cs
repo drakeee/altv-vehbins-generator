dabe5b62590c312d2e0105e9c5d6c0f692404a8e
drake <drakedrift@gmail.com>
2023-01-16 19:49:43 +0100

    Fix some exception

--- altv-vehbins-generator/VehicleMods.cs
+++ altv-vehbins-generator/VehicleMods.cs
@@ -27,49 +27,104 @@ namespace AltV.Generator
             VMT_WING_L = 8,
             VMT_WING_R = 9,
             VMT_ROOF = 10,
-            VMT_ENGINE = 11,
-            VMT_BRAKES = 12,
-            VMT_GEARBOX = 13,
-            VMT_HORN = 14,
-            VMT_SUSPENSION = 15,
-            VMT_ARMOUR = 16,
-            VMT_TURBO = 18,
-            VMT_XENON_LIGHTS = 22,
-            //FrontWheels = 23,
-            //BackWheels = 24,
-            VMT_PLTHOLDER = 25,
-            VMT_PLTVANITY = 26,
-            VMT_INTERIOR1 = 27,
-            VMT_INTERIOR2 = 28,
-            VMT_INTERIOR3 = 29,
-            VMT_INTERIOR4 = 30,
-            VMT_INTERIOR5 = 31,
-            VMT_SEATS = 32,
-            VMT_STEERING = 33,
-            VMT_KNOB = 34,
-            VMT_PLAQUE = 35,
-            VMT_ICE = 36,
-            VMT_TRUNK = 37,
-            VMT_HYDRO = 38,
-            VMT_ENGINEBAY1 = 39,
-            VMT_ENGINEBAY2 = 40,
-            VMT_ENGINEBAY3 = 41,
-            VMT_CHASSIS2 = 42,
-            VMT_CHASSIS3 = 43,
-            VMT_CHASSIS4 = 44,
-            VMT_CHASSIS5 = 45,
-            VMT_DOOR_L = 46,
-            VMT_DOOR_R = 47,
-            VMT_LIVERY_MOD = 48,
-            VMT_WHEELS_REAR_OR_HYDRAULICS = 49
-
-            //VMT_LIGHTBAR = 35,
-            //VMT_NITROUS = 42,
-            //VMT_SUBWOOFER = 44,
-            //VMT_TYRE_SMOKE = 45,
-            //VMT_HYDRAULICS = 46,
-            //VMT_WHEELS = 48,
+            VMT_PLTHOLDER = 11,
+            VMT_PLTVANITY = 12,
+            VMT_INTERIOR1 = 13,
+            VMT_INTERIOR2 = 14,
+            VMT_INTERIOR3 = 15,
+            VMT_INTERIOR4 = 16,
+            VMT_INTERIOR5 = 17,
+            VMT_SEATS = 18,
+            VMT_STEERING = 19,
+            VMT_KNOB = 20,
+            VMT_PLAQUE = 21,
+            VMT_ICE = 22,
+            VMT_TRUNK = 23,
+            VMT_HYDRO = 24,
+            VMT_ENGINEBAY1 = 25,
+            VMT_ENGINEBAY2 = 26,
+            VMT_ENGINEBAY3 = 27,
+            VMT_CHASSIS2 = 28,
+            VMT_CHASSIS3 = 29,
+            VMT_CHASSIS4 = 30,
+            VMT_CHASSIS5 = 31,
+            VMT_DOOR_L = 32,
+            VMT_DOOR_R = 33,
+            VMT_LIVERY_MOD = 34,
+            VMT_LIGHTBAR = 35,
+            VMT_ENGINE = 36,
+            VMT_BRAKES = 37,
+            VMT_GEARBOX = 38,
+            VMT_HORN = 39,
+            VMT_SUSPENSION = 40,
+            VMT_ARMOUR = 41,
+            VMT_NITROUS = 42,
+            VMT_TURBO = 43,
+            VMT_SUBWOOFER = 44,
+            VMT_TYRE_SMOKE = 45,
+            VMT_HYDRAULICS = 46,
+            VMT_XENON_LIGHTS = 47,
+            VMT_WHEELS = 48,
+            VMT_WHEELS_REAR_OR_HYDRAULICS = 49,
+            VMT_HOOD = 50 //maybe?
         };
+        //enum ModType
+        //{
+        //    UNKNOWN = -1,
+        //    VMT_SPOILER = 0,
+        //    VMT_BUMPER_F = 1,
+        //    VMT_BUMPER_R = 2,
+        //    VMT_SKIRT = 3,
+        //    VMT_EXHAUST = 4,
+        //    VMT_CHASSIS = 5,
+        //    VMT_GRILL = 6,
+        //    VMT_BONNET = 7,
+        //    VMT_WING_L = 8,
+        //    VMT_WING_R = 9,
+        //    VMT_ROOF = 10,
+        //    VMT_ENGINE = 11,
+        //    VMT_BRAKES = 12,
+        //    VMT_GEARBOX = 13,
+        //    VMT_HORN = 14,
+        //    VMT_SUSPENSION = 15,
+        //    VMT_ARMOUR = 16,
+        //    VMT_TURBO = 18,
+        //    VMT_XENON_LIGHTS = 22,
+        //    //FrontWheels = 23,
+        //    //BackWheels = 24,
+        //    VMT_PLTHOLDER = 25,
+        //    VMT_PLTVANITY = 26,
+        //    VMT_INTERIOR1 = 27,
+        //    VMT_INTERIOR2 = 28,
+        //    VMT_INTERIOR3 = 29,
+        //    VMT_INTERIOR4 = 30,
+        //    VMT_INTERIOR5 = 31,
+        //    VMT_SEATS = 32,
+        //    VMT_STEERING = 33,
+        //    VMT_KNOB = 34,
+        //    VMT_PLAQUE = 35,
+        //    VMT_ICE = 36,
+        //    VMT_TRUNK = 37,
+        //    VMT_HYDRO = 38,
+        //    VMT_ENGINEBAY1 = 39,
+        //    VMT_ENGINEBAY2 = 40,
+        //    VMT_ENGINEBAY3 = 41,
+        //    VMT_CHASSIS2 = 42,
+        //    VMT_CHASSIS3 = 43,
+        //    VMT_CHASSIS4 = 44,
+        //    VMT_CHASSIS5 = 45,
+        //    VMT_DOOR_L = 46,
+        //    VMT_DOOR_R = 47,
+        //    VMT_LIVERY_MOD = 48,
+        //    VMT_WHEELS_REAR_OR_HYDRAULICS = 49
+
+        //    //VMT_LIGHTBAR = 35,
+        //    //VMT_NITROUS = 42,
+        //    //VMT_SUBWOOFER = 44,
+        //    //VMT_TYRE_SMOKE = 45,
+        //    //VMT_HYDRAULICS = 46,
+        //    //VMT_WHEELS = 48,
+        //};
 
         class ModKit
         {
@@ -102,6 +157,8 @@ namespace AltV.Generator
                 int kitId = Convert.ToInt32(kitData["id"]["@value"]);
                 string kitName = kitData["kitName"].ToString();
 
+                Utils.Log.Info($"Processing \"{kitName}\" kit");
+
                 if (tempList.Where(p => p.KitName == kitName).ToList().Count > 0)
                     continue;
 
@@ -117,6 +174,7 @@ namespace AltV.Generator
                 foreach (var modItem in new string[] { "visibleMods", "statMods" })
                 {
                     var mod = kitData[modItem];
+
                     if (
                         mod.Type == JTokenType.Null ||
                         mod == null ||
@@ -129,19 +187,20 @@ namespace AltV.Generator
 
                     foreach (var m in mod)
                     {
-                        var typeId = Convert.ToInt32(Enum.Parse(typeof(ModType), m["type"].ToString()));
-                        if (!kit.Kits.ContainsKey(typeId))
-                            kit.Kits.Add(typeId, new List<int>());
+                        if(m["type"] != null)
+                        {
+                            var typeId = Convert.ToInt32(Enum.Parse(typeof(ModType), m["type"].ToString()));
+                            if (!kit.Kits.ContainsKey(typeId))
+                                kit.Kits.Add(typeId, new List<int>());
 
-                        kit.Kits[typeId].Add(index);
-                        index++;
+                            kit.Kits[typeId].Add(index);
+                            index++;
+                        }
                     }
 
                     kit.Kits = kit.Kits.OrderBy(x => x.Key).ToDictionary(x => x.Key, x => x.Value);
                 }
                 tempList.Add(kit);
-
-                Utils.Log.Info($"Processing \"{kitName}\" kit");
             }
 
             return tempList;
